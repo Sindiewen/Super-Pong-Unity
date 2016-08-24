73894e93c362b172b6586eaa6860f031ed859f34
Rachel Vancleave <sindiewen@gmail.com>
2016-08-24 13:59:04 -0700

    Wow, Managed to get a respawn timer working... Months later lol.
    It runs terribly though i'll need to fix that

--- Super Pong/Assets/Scripts/Gameplay/Brick_Controller.cs
+++ Super Pong/Assets/Scripts/Gameplay/Brick_Controller.cs
@@ -10,13 +10,23 @@ public class Brick_Controller : MonoBehaviour
 	//public GameObject[] rightBrickHolder 	= GameObject.FindGameObjectsWithTag ("Right Brick");
 	
 	public bool 	brickTimerSelect 	= false;	// Mode to turn on the mode to have bricks respawn mid-game after a set time
-	public float 	brickTimerSeconds	= 500;		// Brick respawn timer
-	
+	//public float 	brickTimerSeconds	= 500;		// Brick respawn timer
+	public float 	brickRespawn		= 5f;
+
+
 	// Private Variables --- DO NOT EDIT
-	private float brickTimerCountdown;
+	//private float brickTimerCountdown;
+
+	private MeshRenderer mesh;
+	private BoxCollider2D box;
+
 	
 	void Start () 
 	{
+
+		mesh 	= GetComponent<MeshRenderer>();
+		box 	= GetComponent<BoxCollider2D>();
+
 		// if (brickTimerSelect == false)
 		// {
 		// 	return;
@@ -26,20 +36,24 @@ public class Brick_Controller : MonoBehaviour
 		// {
 		// 	respawnBrick();
 		// }
-		
+
 		//brickTimerSelect = false;	// Ensures at gamestart, the bricks will not respawn
 		
-		respawnBrick();				// Ensures the bricks are enables at the start of the scene
+	//	respawnBrick();				// Ensures the bricks are enables at the start of the scene
 	}
 	
 	void disableBrick()
 	{
-		gameObject.SetActive(false);	// Disables the brick attached to this script
+		mesh.enabled = !mesh.enabled;
+		box.enabled = !box.enabled;
+		//gameObject.SetActive(false);	// Disables the brick attached to this script
 	}
 	
 	void respawnBrick()
 	{
-		gameObject.SetActive(true); 	// Re-enables the bricks attached to this script
+		mesh.enabled = !mesh.enabled;
+		box.enabled = !box.enabled;
+		//gameObject.SetActive(true); 	// Re-enables the bricks attached to this script
 	}
 	
 	public void OnCollisionEnter2D(Collision2D col)	// When a ball collides to the brick...
@@ -50,28 +64,30 @@ public class Brick_Controller : MonoBehaviour
 		
 		if (brickTimerSelect == true)	// if true...
 		{
-			brickTimerRespawn();		// Calls the brick timer respawn function
+			Invoke("respawnBrick", brickRespawn);
+			//brickTimerRespawn();		// Calls the brick timer respawn function
 		}
 		
 	}
 	
 	public void brickTimerRespawn()
 	{
-		
-		brickTimerCountdown = brickTimerSeconds;	// Sets the timer countdown to = the seconds the countdown will be
-		
-		brickTimerCountdown -= Time.deltaTime;
-		
-		while (brickTimerCountdown > 0)
-		{
-			brickTimerCountdown--;
-		}
-		
-		if (brickTimerCountdown <= 0)	// if countdown < 0...
-		{
-			gameObject.SetActive(true);	// Sets the gameobject to true
-		}
-		
+
+		Invoke("respawnBrick", brickRespawn);
+
+//		brickTimerCountdown = brickTimerSeconds;	// Sets the timer countdown to = the seconds the countdown will be
+//		
+//		brickTimerCountdown -= Time.deltaTime;
+//		
+//		while (brickTimerCountdown > 0)
+//		{
+//			brickTimerCountdown--;
+//		}
+//		
+//		if (brickTimerCountdown <= 0)	// if countdown < 0...
+//		{
+//			gameObject.SetActive(true);	// Sets the gameobject to true
+//		}
+//		
 	}
-	
 }
