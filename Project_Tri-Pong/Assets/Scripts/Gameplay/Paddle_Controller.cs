cbf79331588d64eedbe4b2dea267466d191d0f70
Rachel Vancleave <sindiewen@gmail.com>
2017-03-05 17:52:03 -0800

    Adding game controller class to derive from

--- Project_Tri-Pong/Assets/Scripts/Gameplay/Paddle_Controller.cs
+++ Project_Tri-Pong/Assets/Scripts/Gameplay/Paddle_Controller.cs
@@ -5,10 +5,19 @@ public class Paddle_Controller : MonoBehaviour
 {
 	// Public Variables
 	public float paddleSpeed 	= 30.0f; 		// The speed the paddles will move
+	public float maxHeight = 4.0f;
+	public float minHeight = -4.0f;
 
 	// Private Variables
 	private string paddleAxis 	= "Vertical";	// The axis the paddles will move
+	private Rigidbody2D paddleRB2D;
 	
+	// Upon the object being created
+	void Awake()
+	{
+		// Creates an instance of the RIgidBody2D object
+		paddleRB2D = GetComponent<Rigidbody2D>();
+	}
 	
 	// Updates every fixed frame
 	void FixedUpdate()
@@ -16,8 +25,12 @@ public class Paddle_Controller : MonoBehaviour
 		// Defines the paddlemovement
 		float moveInput = Input.GetAxisRaw(paddleAxis);
 			
+			
 		// Tells the rigidbody component to move the gameobject
-		GetComponent<Rigidbody2D>().velocity = new Vector2(0 , moveInput) * paddleSpeed;
+		paddleRB2D.velocity = new Vector2(0 , moveInput) * paddleSpeed;
+
+
+		Mathf.Clamp(transform.position.y, minHeight, maxHeight);		
 		
 	}
 }
