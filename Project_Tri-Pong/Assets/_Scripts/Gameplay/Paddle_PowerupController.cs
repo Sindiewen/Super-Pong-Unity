e5eae1d720032b6d01cf04a7bfa899d347d4e590
Rachel Vancleave <sindiewen@gmail.com>
2017-03-22 22:22:01 -0700

    Successfully added 1 powerup to the project.

--- Project_Tri-Pong/Assets/_Scripts/Gameplay/Paddle_PowerupController.cs
+++ Project_Tri-Pong/Assets/_Scripts/Gameplay/Paddle_PowerupController.cs
@@ -1,32 +1,47 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle_PowerupController : MonoBehaviour 
 {
 	// Checks if paddle picked up any powerups
+	
+	// private variables
+	// Private Powerup References
+	private bool PSIActive = false;		// PaddleSizeIncrease Variable
+	
 
-	void Start()
-	{
-		PU_PaddleSizeIncrease PSI = this.gameObject.AddComponent<PU_PaddleSizeIncrease>() as PU_PaddleSizeIncrease;
 
-	}
-	void OnTriggerEnter2D(Collider2D col)
+	void OnCollisionEnter2D(Collision2D col)
 	{
-		if (col.gameObject.tag == "Paddle Size Increase")
+		// Increase Paddle Size:
+		// If collided, increase paddle size
+		if(!PSIActive && col.gameObject.tag == "Paddle Size Increase")
 		{
-			// Adds the Paddle Size Increase Powerup
-			Debug.Log("Adding AddComponent");
-			PU_PaddleSizeIncrease PSI = this.gameObject.AddComponent<PU_PaddleSizeIncrease>() as PU_PaddleSizeIncrease;
-
-			//Destroy(PSI, 100.0f);
+			// Power Up is active
+			PSIActive = true;
+			
+			// Starts the coroutine of the paddle size increase
+			StartCoroutine(_PaddleSizeIncrease(col));
 		}
 	}
 
-	// Returns the current name of the gameObject
-	public string GetObjectName()
+	IEnumerator _PaddleSizeIncrease(Collision2D col)
 	{
-		return this.gameObject.name;
+		// Destroys the collided gameObject
+		//Destroy(col.gameObject);
+		
+		// Increases the size of the paddle
+		this.transform.localScale += new Vector3(0.0f, 2.5f, 0.0f);
+
+		// Wait 5 seconds before reverting the paddles back to their previous state	
+		yield return new WaitForSeconds(5.0f);
+
+		// Returs the size of the paddle
+		this.transform.localScale += new Vector3(0.0f, -2.0f, 0.0f);
+		
+		// The powerup is nolonger active
+		PSIActive = false;
 	}
 
 }
